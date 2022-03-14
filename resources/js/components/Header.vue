40d232a66b3c615a096baa40c76c65bf9e125a8b
Maccio92 <valerio362@gmail.com>
2022-03-14 15:44:50 +0100

    first commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item"  
+                    v-for="(item, index) in menuItems"
+                    :key="index"
+                    >
+                    <router-link class="nav-link" :to="{name: item.page}">
+                        {{ item.label }}
+
+                    </router-link>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+        
+        menuItems: [
+                    {
+                        label: 'Home',
+                        page: 'home',
+                    },
+                    {
+                        label: 'About',
+                        page: 'about',
+                    },
+                    {
+                        label: 'Contacts',
+                        page: 'contacts',
+                    },
+                ],
+    }
+    },
+}
+</script>
+
+<style lang="scss">
+</style>
